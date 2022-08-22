95e9c44d1f5b5405d4e779596076506f85047ebf
Austin <austinccy81@gmail.com>
2022-08-22 13:46:38 +0100

    add login and register

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-let items = document.querySelectorAll('.carousel .carousel-item')
+//firebase config
+const firebaseConfig = {
+    apiKey: "AIzaSyCf7V9yh1ldttFwXbgTbrb_PqGP4eT2PAc",
+    authDomain: "fb-login-ac.firebaseapp.com",
+    projectId: "fb-login-ac",
+    storageBucket: "fb-login-ac.appspot.com",
+    messagingSenderId: "441044041212",
+    appId: "1:441044041212:web:3945be8eb0afb566448679"
+  };
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth()
+const database = firebase.database()
 
+//define DOM objects
+const btnLogin = document.getElementById("btn-login")
+const errorMsg = document.getElementById("error-message")
+const items = document.querySelectorAll('.carousel .carousel-item')
+
+//event listener
+btnLogin.addEventListener("click",()=>{login()})
+
+//carousel control
 items.forEach((el) => {
     const minPerSlide = 4
     let next = el.nextElementSibling
@@ -13,3 +33,57 @@ items.forEach((el) => {
         next = next.nextElementSibling
     }
 })
+
+function login () {
+    email = document.getElementById('email').value
+    password = document.getElementById('password').value
+    if (validate_password(password) == false) {
+      errorMsg.innerText = "please enter password with 6 digits or more"
+      return
+    }
+    if (validate_email(email) == false) {
+      errorMsg.innerText = "invalid email"
+      return
+    }
+  
+    auth.signInWithEmailAndPassword(email, password)
+    .then(function() {
+      const user = auth.currentUser
+      const database_ref = database.ref()
+      database_ref.child('users/' + user.uid)
+      sessionStorage.setItem("username",user.uid)
+      window.location = "./portal";
+      
+    })
+    .catch(function(error) {
+      if (error.code === "auth/wrong-password"){
+        errorMsg.innerText="incorrect password"
+      }else if(error.code === "auth/user-not-found"){
+        errorMsg.innerText="no registered user with this email address"
+      }else{
+        errorMsg.innerText=error.message
+      }}
+    )
+    
+  }
+  
+  function validate_email(email) {
+    expression = /^[^@]+@\w+(\.\w+)+\w$/
+    if (expression.test(email) == true) {
+      return true
+    } else {
+      return false
+    }
+  }
+  
+  function validate_password(password) {
+    if (password.length < 6) {
+      return false
+    } else {
+      return true
+    }
+  }
+  
+  function register(){
+    window.location = "./register/"
+  }
---
+++ portal/portal.js
@@ -0,0 +1,50 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCf7V9yh1ldttFwXbgTbrb_PqGP4eT2PAc",
+  authDomain: "fb-login-ac.firebaseapp.com",
+  projectId: "fb-login-ac",
+  storageBucket: "fb-login-ac.appspot.com",
+  messagingSenderId: "441044041212",
+  appId: "1:441044041212:web:3945be8eb0afb566448679"
+};
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth()
+const database = firebase.database()
+const username = sessionStorage.getItem("username")
+
+const getUserData=()=>{
+  database.ref(`/users/${username}/`).once('value')
+  .then(result=>{
+  document.getElementById("greeting").innerText = "Hello, "+result.val().username
+  if (!result.val().plan) {
+      document.getElementById("plan").innerText = "Plan: No plans subscribed"
+      subscribeDOM.style.display="flex"
+      subscribeDOM.style.opacity=1
+  }else {
+      document.getElementById("plan").innerText = "Plan: "+result.val().plan
+  }
+  if (!result.val().program) {
+      document.getElementById("program").innerText = "Program: No programs enrolled"
+      if (result.val().plan){
+          enrollDOM.style.display="block"
+          setTimeout(()=>enrollDOM.style.opacity=1,200) 
+      }
+  }else {
+      document.getElementById("program").style.display="none"
+      document.getElementById("dashboard-program").innerText = result.val().program
+      dashboardDOM.style.display = "block"
+  }
+})}
+if (username){
+  getUserData()
+}else{
+  console.log("Not logged in")
+  window.location.href="../"
+}
+
+
+const logout=()=>{
+  auth.signOut().then(()=>{
+      sessionStorage.clear()
+      window.location.href="../"
+  })
+}
